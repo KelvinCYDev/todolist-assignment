94910eb9c0352af7fbf7ef97d044ec3556329ae0
KelvinCY <kelvincyhk@gmail.com>
2024-09-03 17:59:55 -0700

    Fix: Add a key prop to the TodoItem component in the TodoList.tsx file.
    Missing key prop in TodoList component when rendering TodoItem components. Adding a unique key prop helps React efficiently update the list when items change, improving performance and preventing potential rendering issues.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,28 +3,34 @@ import { Todo } from "@/types/todo";
 import TodoItem from "./TodoItem";
 
 interface TodoListProps {
-    todos: Todo[];
-    deleteTodo: (id: number) => void;
-    toggleComplete: (id: number) => void;
-    toggleUrgent: (id: number) => void;
+  todos: Todo[];
+  deleteTodo: (id: number) => void;
+  toggleComplete: (id: number) => void;
+  toggleUrgent: (id: number) => void;
 }
 
 /*
  * TodoList: Create a ul element and render a TodoItem element for each todo supplied
  */
-function TodoList({ todos, deleteTodo, toggleComplete, toggleUrgent }: TodoListProps) {
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <TodoItem
-                    todo={todo}
-                    deleteTodo={deleteTodo}
-                    toggleComplete={toggleComplete}
-                    toggleUrgent={toggleUrgent}
-                />
-            ))}
-        </ul>
-    );
+function TodoList({
+  todos,
+  deleteTodo,
+  toggleComplete,
+  toggleUrgent,
+}: TodoListProps) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleComplete={toggleComplete}
+          toggleUrgent={toggleUrgent}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
